e9ad00c32ca120e5733859e57938e074a54e154b
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-02-06 21:15:48 +0530

    Time: 4 ms (5.14%) | Memory: 41.3 MB (48.41%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder str1 = new StringBuilder();
-        StringBuilder str2 = new StringBuilder();
-        for(String x : word1)
-            str1.append(x);
-        for(String x : word2)
-            str2.append(x);
-        System.out.println(str1);
-        System.out.println(str2);
-        return (str1.toString().equals(str2.toString()));
-
+        int oIdx1 = 0, oIdx2 = 0;
+        int inX=0, inY=0;
+        while(oIdx1<word1.length && oIdx2<word2.length) {
+            if(word1[oIdx1].charAt(inX)!=word2[oIdx2].charAt(inY))
+                return false;
+            if(inX < word1[oIdx1].length())
+                inX++;
+            if(inY < word2[oIdx2].length())
+                inY++;
+            if(inX == word1[oIdx1].length()){
+                inX = 0;
+                oIdx1++;
+            }
+            if(inY == word2[oIdx2].length()){
+                inY = 0;
+                oIdx2++;
+            }
+        }
+        if(oIdx1 == word1.length && oIdx2 ==word2.length)
+            return true;
+        return false;
     }
 }
