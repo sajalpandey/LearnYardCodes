4d80f0af89decce5bbe75447d7b67745abb79383
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-02-18 17:49:16 +0530

    Time: 1 ms (99.29%) | Memory: 45 MB (59.14%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,23 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        int last_index = nums.length-1;
-        int reach[] = new int[nums.length];
-        Arrays.fill(reach,Integer.MAX_VALUE);
-        reach[last_index] = 0;
-        for(int i=last_index-1; i>=0; i--) {
-            int value = nums[i];
-            System.out.println("value = "+value);
-            int mn = Integer.MAX_VALUE;
-            for(int j=1; j<=value && j+i<=last_index; j++)
-                mn = Math.min(mn,reach[i+j]);
-            System.out.println("mn = "+mn);
-            if(mn!=Integer.MAX_VALUE)
-            reach[i] = 1 + mn;
-        }
+        int n = nums.length;
+        int lastjumpindex = 0, maxjump = 0;
+        int result = 0;
+        if(n==1)
+            return 0;
+        for(int i=0; i<n; i++) {
+            // This is the max we can reach
+            maxjump = Math.max(maxjump, i+nums[i]);
 
-        for(int ele : reach)
-            System.out.print(ele+" ");
-        System.out.println();
-        return reach[0];
+            // This means we have traversed one window
+            if(i==lastjumpindex) {
+                lastjumpindex = maxjump;
+                result++;
+                if(maxjump >= n-1)
+                    return result;
+            }
+
+            
+        }
+        return result;
     }
 }
