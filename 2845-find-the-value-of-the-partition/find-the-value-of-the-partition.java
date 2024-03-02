66021ac69c02401e40f6facf00b4fa51789067b3
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-03-02 21:19:07 +0530

    Time: 18 ms (100.00%) | Memory: 55.5 MB (31.25%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.java
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.java
@@ -1,8 +1,9 @@
 class Solution {
     public int findValueOfPartition(int[] nums) {
+        int n =  nums.length;
         Arrays.sort(nums);
         int result = Integer.MAX_VALUE;
-        for(int i=1; i<nums.length; i++) {
+        for(int i=1; i<n; ++i) {
             result = Math.min(result,(nums[i]-nums[i-1]));
         }
         return result;
