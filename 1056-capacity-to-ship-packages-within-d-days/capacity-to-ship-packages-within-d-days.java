62f7bdd656d3f3395097848fde0bd7f442f00b1c
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-03-08 21:50:17 +0530

    Time: 28 ms (5.07%) | Memory: 46.8 MB (90.48%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canBeShipped(int[] weights, int mid, int days){
+        int d = 1,w = 0;
+        for(int ele : weights){
+            if((w + ele) <= mid) {
+                w += ele;
+            } else {
+                d++;
+                w = 0;
+                w += ele;
+            }
+        }
+        System.out.println((d < days ? true : false) + " mid = "+mid+" --d = "+d+" days = "+days);
+        return (d <= days ? true : false);
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int ans = 1;
+        int lo = 1, hi = 0;
+        for(int ele : weights) {
+            hi += ele;
+            lo = Math.max(lo, ele);
+        }
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if(canBeShipped(weights,mid, days)) {
+                ans = mid;
+                hi = mid - 1;
+            } else
+                lo = mid + 1;
+        }
+        return ans;
+    }
+}
