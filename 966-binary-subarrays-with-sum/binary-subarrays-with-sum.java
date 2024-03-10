577ef05806da015d5f1deb00f02917cd01adb070
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-03-10 13:13:42 +0530

    Time: 23 ms (32.72%) | Memory: 53.2 MB (5.12%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -5,12 +5,12 @@ class Solution {
         prefix.put(0,1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if(sum >= goal){
+            //if(sum >= goal){
                 int rem = sum - goal;
                 if (prefix.containsKey(rem)) {
                     result += prefix.get(rem);
                 }
-            }
+            //}
             prefix.put(sum, prefix.getOrDefault(sum, 0) + 1);
         }
         return result;
