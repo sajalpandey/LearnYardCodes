4769006ce5c4cc08c2e62203dd7bc240aa8248d3
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-03-26 23:17:45 +0530

    Time: 23 ms (31.35%) | Memory: 81.8 MB (49.53%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.java
+++ 1622-max-value-of-equation/max-value-of-equation.java
@@ -2,16 +2,16 @@ class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
         int result = Integer.MIN_VALUE;
         Deque<Integer> dq = new LinkedList<>();
-        for(int i=0; i<points.length; i++) {
+        for(int i=0; i<points.length; ++i) {
 
             // First check if deque contains max coordinate of within the range
             // like give xj - xi <= k
             //yi + yj + |xi - xj|
             // yi + yj + xj - xi ==> yj + xj + (yi - xi)
-            while(dq.size() > 0 && points[i][0] - points[dq.peekFirst()][0] > k)
+            while(!dq.isEmpty() && points[i][0] - points[dq.peekFirst()][0] > k)
                 dq.removeFirst();
             
-            if(dq.size() > 0) {
+            if(!dq.isEmpty()) {
                 int j = dq.peekFirst();
                 int ans = points[i][1] + points[i][0] + (points[j][1] - points[j][0]);
                 result = Math.max(result, ans); 
@@ -20,7 +20,7 @@ class Solution {
             // Insert this point to maintains that maximum yi - xi should be maximum
             // in deque front
             int value = points[i][1] - points[i][0];
-            while(dq.size() > 0 && (points[dq.peekLast()][1] - points[dq.peekLast()][0]) <= value)
+            while(!dq.isEmpty() && (points[dq.peekLast()][1] - points[dq.peekLast()][0]) <= value)
                 dq.removeLast();
             dq.addLast(i);
         }
