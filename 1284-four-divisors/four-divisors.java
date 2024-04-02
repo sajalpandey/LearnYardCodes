8b144901256691d350bd0a9779381682d07655ff
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-02 23:49:04 +0530

    Time: 27 ms (70.98%) | Memory: 44.9 MB (40.49%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -10,18 +10,16 @@ class Solution {
                 power += 1;
             }
             result *= (power+1);
-            //ans.put(i, power);
         }
         if(n > 1)
             result *= 2;
-        //System.out.println("result = "+result);
         return result;
     }
     public static int sumOfFactors(int n) {
         // Initialize sum to store the sum of factors
         int sum = 0;
         // Iterate from 1 to n
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; ++i) {
             // If i is a divisor of n, add it to the sum
             if (n % i == 0) {
                 sum += i;
@@ -30,12 +28,29 @@ class Solution {
         return sum;
     }
     public int sumFourDivisors(int[] nums) {
-        int sum = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(primeFactors(nums[i])==4) {
-                sum += sumOfFactors(nums[i]);
+        // find max element
+        int mx = 0;
+        for (int num : nums) {
+            mx = Math.max(mx, num);
+        }
+
+        int[] countDivisors = new int[mx + 1];//// for storing divisors and their sum
+        int[] sumDivisors = new int[mx + 1];
+
+        for (int i = 1; i <= mx; i++) {
+            for (int j = i; j <= mx; j += i) {
+                countDivisors[j]++;
+                sumDivisors[j] += i;
             }
         }
+
+        int sum = 0;
+        for (int num : nums) {
+            if (countDivisors[num] == 4) {
+                sum += sumDivisors[num];
+            }
+        }
+
         return sum;
     }
 }
