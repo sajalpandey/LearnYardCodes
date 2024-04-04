5b8a05ea328b4bcfc919eb3d73a446fad392a773
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-04 20:56:49 +0530

    Time: 11 ms (59.78%) | Memory: 45.5 MB (21.23%) - LeetSync

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -9,8 +9,7 @@ class Solution {
         for(int ele : deck)
             map.put(ele,map.getOrDefault(ele,0)+1);
         int gcd = 0;
-        for(int key : map.keySet()) {
-            int val = map.get(key);
+        for(int val : map.values()) {
             gcd = findGcd(gcd, val);
         }
         if(gcd > 1)
