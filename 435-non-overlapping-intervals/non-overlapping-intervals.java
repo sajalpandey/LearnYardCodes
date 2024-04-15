2370596fd9a35be4b4a334119825adc095481ea4
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-15 10:22:18 +0530

    Time: 45 ms (95.48%) | Memory: 73.1 MB (85.12%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,26 +1,16 @@
 class Solution {
-    class Pair {
-        int key;
-        int value;
-        Pair(int k, int v) {
-            key = k;
-            value = v;
-        }
-    }
     public int eraseOverlapIntervals(int[][] intervals) {
-        Stack<Pair> st = new Stack<>();
         Arrays.sort(intervals, (a,b) -> (a[1]-b[1]));
-        
+
         int count = 0;
-        for(int i=0; i<intervals.length; ++i) {
-            if(!st.isEmpty() && intervals[i][0] < st.peek().value) {
-                //int key = st.peek().key;
+        int i = 0, j = 1;
+        while(j < intervals.length) {
+            if(intervals[j][0] < intervals[i][1])
                 count++;
-                //st.pop();
-                //st.push(new Pair(key, intervals[i][1]));
-            } else {
-                st.push(new Pair(intervals[i][0], intervals[i][1]));
+            else {
+                i = j;
             }
+            j++;
         }
         return count;
     }
