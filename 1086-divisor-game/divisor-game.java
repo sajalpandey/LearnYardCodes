b82d71399c2cbd5c6ad3aa49280dac2d5792d6c0
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-23 20:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (15.89%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,74 +1,26 @@
 class Solution {
-    public boolean divisorGameHelper(int n) {
+    public boolean divisorGameHelper(int n, int[] dp) {
         if(n == 1)
             return false;
-        
+        if(dp[n]!=-1) {
+            if(dp[n] == 0)
+                return false;
+            return true;
+        }
         for(int i=1; i<n; i++) {
             if(n%i == 0) {
-                boolean flag = divisorGameHelper(n-i);
-                return flag == false ? true : false;
+                boolean flag = divisorGameHelper(n-i, dp);
+                flag = flag == false ? true : false;
+                dp[n] = 1;
+                return flag;
             }
         }
+        dp[n] = 0;
         return false;
     }
     public boolean divisorGame(int n) {
-        return divisorGameHelper(n);
-        //return true;
-        // n = 3 (false)
-        // choose num 1 to 2
-        // Alice choses 1
-        // new n = 3-1 = 2
-        // 1 to 1
-        // bob chose 1
-        // new n = 2-1 = 1
-        // Alice false
-
-        // repetative work + optimal substructure (problem can be broken down to smaller problems)
-
-        // n = 4 --> true
-        // Alice choses from 1 to 3
-        // 1
-        // new n = 3
-        // Bob 1
-        // new n = 2
-        // alice 1
-        // new n = 1
-
-        // n = 4 (false)
-        // Alice chose 2
-        // new n = 2
-        // Bob 1
-        // new n = 1
-
-        // n = 5 (false)
-        // Alice 1
-        // new n = 4
-        // Bob n 1
-        // new n = 3
-        // Alice 1
-        // new n = 2
-        // Bob 1
-        // new n = 1
-
-        // n = 5 (true)
-        // Alice 1
-        // new n = 4
-        // Bob 2
-        // new n = 2
-        // Alice 1
-        // new n = 1
-
-        // n = 6 (true)
-        // Alice 1
-        // new n = 5
-        // Bob 1
-        // new n = 4
-        // Alice 1
-        // new n = 3
-        // Bob 1
-        // new n = 2
-        // Alice 1
-        // new n = 1
-
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return divisorGameHelper(n, dp);
     }
 }
