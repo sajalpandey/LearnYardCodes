3ad335ba261bc2a939816bdd943cf2301110ce8b
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-23 09:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (15.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -11,8 +11,19 @@ class Solution {
         return dp[n];
     }
     public int climbStairs(int n) {
+        if(n <= 2)
+            return n;
+        //Recursion + memoisation =  TOP DOWN APPRAOCH
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return climbStairsHelper(n, dp);
+        // Arrays.fill(dp, -1);
+        // return climbStairsHelper(n, dp);
+
+        //BOTTOM UP APPROACH
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 }
