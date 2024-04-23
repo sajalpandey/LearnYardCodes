2e2aca82dff228c2e95578913211a7b590ee98d5
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-23 09:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (90.34%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int helper(int[] nums, int i, int[] dp) {
+        if(i >= nums.length) {
+            return 0;
+        }
+        if(dp[i] != -1)
+            return dp[i];
+
+        //Robber choose to rob this house, then he can rob to next to next house
+        int sum1 = nums[i] + helper(nums, i+2, dp);
+        
+        //Robber decided not to rob this house. then he can rob to next house
+        int sum2 = helper(nums, i+1, dp);
+        dp[i] = Math.max(sum1, sum2);
+        return dp[i];
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(nums,0,dp);
+    }
+    /**
+    Recursion time complexity os O(2^n) & SC: O(2^n)
+     */
+}
