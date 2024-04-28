c8bccefa8652f9ddfd2b95b7c9b63c4447be74e9
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-04-28 20:03:20 +0530

    Time: 29 ms (18.86%) | Memory: 50.6 MB (72.47%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestCommonSubsequenceHelper(int i, int j, String text1, String text2) {
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+        
+        if(text1.charAt(i) == text2.charAt(j)) {
+            return (1 + longestCommonSubsequenceHelper(i+1, j+1, text1, text2));
+        } else {
+            return Math.max(longestCommonSubsequenceHelper(i+1, j, text1, text2), longestCommonSubsequenceHelper(i, j+1, text1, text2));
+        }
+    }
+
+    public int longestCommonSubsequenceTopDown(int i, int j, String text1, String text2, int[][]dp) {
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(text1.charAt(i) == text2.charAt(j)) {
+            dp[i][j] = (1 + longestCommonSubsequenceTopDown(i+1, j+1, text1, text2, dp));
+        } else {
+            dp[i][j] = Math.max(longestCommonSubsequenceTopDown(i+1, j, text1, text2, dp), longestCommonSubsequenceTopDown(i, j+1, text1, text2, dp));
+        }
+        return dp[i][j];
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        //1. Recursive
+        //return longestCommonSubsequenceHelper(0, 0, text1, text2);
+
+        //2. DP : Top Down
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        return longestCommonSubsequenceTopDown(0, 0, text1, text2, dp);
+    }
+}
