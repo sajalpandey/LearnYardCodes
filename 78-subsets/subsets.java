9fe3908713c8a3b778abfc941be0f4946322ef4f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-04 19:56:46 +0530

    Time: 1 ms (59.66%) | Memory: 42.7 MB (28.18%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -11,9 +11,33 @@ class Solution {
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        //result.add(temp);
-        helper(result,temp,nums,0,nums.length);
+        /**
+        [1,2,3] , n = 3 subset = 2^3 = 8 (0 -- 7)
+        [0,1,2] 0,1,2,3,4 --> 100 6--> 110
+        000 - []
+        100 - [1]
+        010 - [2]
+        001 - [3]
+        110 - [1,2]
+        101 - [1,3]
+        011 - [2,3]
+        111 - [1,2,3]
+         */
+        int n = nums.length;
+        for(int i=0; i<(1 << n); i++) {
+            //System.out.println("i = "+i);
+            List<Integer> temp = new ArrayList<>();
+            for(int j=0; j<n; j++) {
+                int bit = (1 << j);
+                //System.out.println("bit = "+bit);
+                if((i & bit) != 0) {
+                    //System.out.println("number = "+i+" --set bit is = "+bit);
+                    //Jth bit is set
+                    temp.add(nums[j]);
+                }
+            }
+            result.add(new ArrayList<>(temp));
+        }
         return result;
     }
 }
