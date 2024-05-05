9c6e554ea7ae53f66bbcb95d6b66540d018c89bf
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-05 13:08:04 +0530

    Time: 38 ms (52.31%) | Memory: 81.3 MB (80.00%) - LeetSync

---
+++ 2700-substring-xor-queries/substring-xor-queries.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] substringXorQueries(String s, int[][] queries) {
+        HashMap<Integer, int[]> map = new HashMap<>();
+        //Generate all possible substring and store in map
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '0') {
+                if(!map.containsKey(0)) {
+                    map.put(0, new int[] {i,i});
+                }
+                continue;
+            }
+            int number = 0;
+
+            for(int j=i; j<s.length() && j<(i+32); j++) {
+                number = (number << 1) + (s.charAt(j) - '0');
+                if(!map.containsKey(number)) 
+                    map.put(number, new int[] {i, j});
+            }
+        }
+        //Now search for every val in map 
+        int[][] result = new int[queries.length][2];
+        for(int i=0; i<queries.length; i++) {
+            int val = queries[i][0] ^ queries[i][1];
+            result[i] = map.getOrDefault(val, new int[]{-1, -1});
+        }
+        return result;
+    }
+}
