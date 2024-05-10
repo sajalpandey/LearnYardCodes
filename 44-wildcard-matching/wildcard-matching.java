0071b2450ed1f817cf650aa739da3e2f9cfee5c9
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-10 10:12:58 +0530

    Time: 30 ms (20.03%) | Memory: 44.9 MB (48.31%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -46,18 +46,53 @@ class Solution {
         return ans;
     }
 
+    public boolean isMatchBottomUp(String s, String p) {
+        int n = s.length(), m = p.length();
+        boolean[][] dp = new boolean[n+1][m+1];
+        for(boolean[] row : dp)
+            Arrays.fill(row, false);
+        //First Base 
+        dp[0][0] = true;
+        //second base case , array is already filled with false
+
+        //Third base case, when i == 0 & j > 0
+        for(int j=1; j<=m; j++) {
+            boolean flag = true;
+            for(int k=1; k<=j; k++)
+                if(p.charAt(k-1) != '*') {
+                    flag = false;
+                    break;
+                }
+            dp[0][j] = flag;
+        }
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p.charAt(j-1) == '*') //either to take * as empty or match char at i
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                else
+                    dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
+    }
+
     public boolean isMatch(String s, String p) {
         //1. Recursion
         //return isMatchHelper(s, p, s.length()-1, p.length()-1);
 
         //2.DP Top Down
         int[][] dp = new int[s.length()+1][p.length()+1];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return isMatchTopDown(s, p, s.length(), p.length(), dp);
+        // for(int[] row : dp)
+        //     Arrays.fill(row, -1);
+        // return isMatchTopDown(s, p, s.length(), p.length(), dp);
 
         //3. DP Bottom Up 
         //First we need to convert our top down solution to 1 bases indexing, then
         //it will be easier to solve bottom up
+
+        return isMatchBottomUp(s, p);
     }
 }
