e3be233ee620132e30d2b924d9ee9cbf3b83654f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-12 22:10:23 +0530

    Time: 65 ms (36.86%) | Memory: 63.1 MB (32.48%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -36,16 +36,41 @@ class Solution {
         return dp[i][buy][count] = profit;
     }
 
+    public int maxProfitBottomUp(int[] prices){
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][3];
+
+        for(int i=n-1; i>=0; i--) {
+            for(int buy=0; buy<=1; buy++) {
+                for(int count=1; count>=0; count--) {
+                    int profit = 0;
+                    if(buy == 1) {
+                        int takeThis = -prices[i] + dp[i+1][0][count];
+                        int skipThis = 0 + dp[i+1][1][count];
+                        profit = Math.max(takeThis, skipThis);
+                    } else {
+                        int sellThis = prices[i] + dp[i+1][1][count+1];
+                        int skipThis = 0 + dp[i+1][0][count];
+                        profit = Math.max(sellThis, skipThis);
+                    }
+                    dp[i][buy][count] = profit;
+                }
+            }
+        }
+        return dp[0][1][0];
+    }
     public int maxProfit(int[] prices) {
         //1. Recursion
         //return maxProfitHelper(prices, 0, 1, 0);
 
         //2. DP Top Down
-        int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int[][] matrix : dp)
-            for(int[] row : matrix)
-                Arrays.fill(row, -1);
-        return maxProfitTopDown(prices, 0, 1, 0, dp);
+        // int n = prices.length;
+        // int[][][] dp = new int[n][2][3];
+        // for(int[][] matrix : dp)
+        //     for(int[] row : matrix)
+        //         Arrays.fill(row, -1);
+        // return maxProfitTopDown(prices, 0, 1, 0, dp);
+
+        return maxProfitBottomUp(prices);
     }
 }
