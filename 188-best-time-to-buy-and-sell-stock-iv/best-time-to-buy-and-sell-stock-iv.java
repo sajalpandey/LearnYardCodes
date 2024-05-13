7219d0056f743b60d972a7e931ade5912c0d99fd
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-13 10:12:24 +0530

    Time: 2 ms (80.23%) | Memory: 44.3 MB (42.86%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -36,16 +36,44 @@ class Solution {
 
         return dp[i][buy][k] = profit;
     }
+
+    public int maxProfitBottomUp(int[] prices, int K) {
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][K+1];
+
+        for(int i=n-1; i>=0; i--) {
+            for(int buy=0; buy<=1; buy++) {
+                for(int k=1; k<=K; k++) {
+                    int profit = 0;
+                    if(buy == 1) {
+                        int buyThis = -prices[i] + dp[i+1][0][k];
+                        int skipThis = 0 + dp[i+1][1][k];
+                        profit = Math.max(buyThis, skipThis);
+                    } else {
+                        int sellThis = prices[i] + dp[i+1][1][k-1];
+                        int skipThis = 0 + dp[i+1][0][k];
+                        profit = Math.max(sellThis, skipThis);
+                    }
+                    dp[i][buy][k] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][K];
+    }
     public int maxProfit(int k, int[] prices) {
         //1. Recursion
         //return maxProfitHelper(prices, k, 0, 1);
 
         //2. DP Top Down
-        int n = prices.length;
-        int[][][] dp = new int[n][2][k+1];
-        for(int[][] matrix : dp)
-            for(int[] row : matrix)
-                Arrays.fill(row, -1);
-        return maxProfitTopDown(prices, k, 0, 1, dp);
+        // int n = prices.length;
+        // int[][][] dp = new int[n][2][k+1];
+        // for(int[][] matrix : dp)
+        //     for(int[] row : matrix)
+        //         Arrays.fill(row, -1);
+        // return maxProfitTopDown(prices, k, 0, 1, dp);
+
+        //3. DP Bottom Up
+        return maxProfitBottomUp(prices, k);
     }
 }
