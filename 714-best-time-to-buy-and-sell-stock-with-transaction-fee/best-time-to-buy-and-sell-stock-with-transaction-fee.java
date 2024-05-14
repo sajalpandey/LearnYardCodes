1973d8e71b83e9115f1db5cc37b81ecc000c2215
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-05-14 09:59:46 +0530

    Time: 20 ms (38.89%) | Memory: 54.7 MB (90.10%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -17,11 +17,38 @@ class Solution {
         }
         return dp[i][buy] = profit;
     }
-    public int maxProfit(int[] prices, int fee) {
+
+    public int maxProfitBottomUp(int[] prices, int fee) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return maxProfitTopDown(prices, 0, 1, fee, dp);
+
+        for(int i=n-1; i>=0; i--) {
+            for(int buy=0; buy<=1; buy++) {
+                int profit = 0;
+                if(buy == 1) {
+                    int buyThis = -prices[i] + dp[i+1][0];
+                    int skipThis = 0 + dp[i+1][1];
+                    profit = Math.max(buyThis, skipThis);
+                } else {
+                    int sellThis = +prices[i] - fee + dp[i+1][1];
+                    int skipThis = 0 +  dp[i+1][0];
+                    profit = Math.max(sellThis, skipThis);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+    public int maxProfit(int[] prices, int fee) {
+
+        //1. DP Top Down
+        // int n = prices.length;
+        // int[][] dp = new int[n+1][2];
+        // for(int[] row : dp)
+        //     Arrays.fill(row, -1);
+        // return maxProfitTopDown(prices, 0, 1, fee, dp);
+
+        //2. DP Bottom Up
+        return maxProfitBottomUp(prices, fee);
     }
 }
