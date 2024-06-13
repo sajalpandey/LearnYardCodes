7922d2f4f624ac24b71856e4ac94153a1b3a820e
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-06-13 16:25:21 +0530

    Time: 2 ms (75.97%) | Memory: 46.1 MB (72.75%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -0,0 +1,41 @@
+class Solution {
+    List<List<Integer>> adj, res;
+    boolean[] vis;
+    public void dfs(int curr, List<Integer> temp, int n) {
+        //System.out.println("curr = "+curr);
+        vis[curr] = true;
+        temp.add(curr);
+        if(curr == n-1) {
+            res.add(new ArrayList<>(temp));
+        }
+
+        //now call dfs for every neighbour
+        for(int nb : adj.get(curr)) {
+            // if(vis[nb] == true)
+            //     continue;
+            dfs(nb, temp, n);
+        }
+        //then backtrack
+        vis[curr] = false;
+        temp.remove(temp.size()-1);
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        res = new ArrayList<>();
+        int n = graph.length;
+        adj = new ArrayList<>();
+        vis = new boolean[n];
+        Arrays.fill(vis, false);
+        for(int i=0; i<n; i++)
+            adj.add(new ArrayList<>());
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<graph[i].length; j++) {
+                adj.get(i).add(graph[i][j]);
+            }
+        }
+        List<Integer> temp = new ArrayList<>();
+        
+        dfs(0, temp, n);
+        return res;
+
+    }
+}
