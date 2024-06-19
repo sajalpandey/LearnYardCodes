49ec91a83191268ec0c8672f677b9c43df2245c3
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-06-19 10:38:57 +0530

    Time: 13 ms (34.33%) | Memory: 44.1 MB (76.71%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,158 +1,46 @@
-class Edge {
-	int node;
-	int weight;
-	
-	Edge(int node, int weight) {
-		this.node = node;
-		this.weight = weight;
-	}
-}
-
-class Node {
-	int node;
-	int dist;
-	
-	Node(int node, int dist) {
-		this.node = node;
-		this.dist = dist;
-	}
-}
 class Solution {
-    static ArrayList<ArrayList<Edge>> adj;
-    static class comp implements Comparator<Node> {
-		public int compare(Node a, Node b) {
-			if(a.dist > b.dist)
-				return 1;
-			else if(a.dist == b.dist && a.node == b.node)
-				return 0;
-			else
-				return -1;
-		}
-	}
-    public static int bellmanFord(int src, int ds, int[][] edges, int n) {
-        int m = edges.length;
-		long[] d = new long[n+1];
-		Arrays.fill(d, Long.MAX_VALUE);
-		
-		d[src] = 0;
-		//Relax all the edges(u,v,weight) V-1 times
-		for(int i=1; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				for(int[] e : edges) {
-					int u = e[0], v = e[1], w = e[2];
-					if(d[u] != Long.MAX_VALUE && (d[u] + w) < d[v]) {
-						d[v] = d[u] + w;
-					}
+    final int inf = (int)1e9;
+    public int[][] floydWarshell(int[][] edges, int n) {
+        int[][] d = new int[n][n]; // 0 to n-1
+		for(int[] row : d)
+			Arrays.fill(row, inf);
 
-                    // if((d[v] + w) < d[u])
-                    //     d[u] = d[v] + w;
-				}
-			}
+		// Initialise the d array properly
+		for(int i = 0; i < n; ++i)
+			d[i][i] = 0;
+		
+		for(int[] e : edges) { // {i, j, w}
+			d[e[0]][e[1]] = e[2];
+			d[e[1]][e[0]] = e[2];
 		}
-		
-		//Perform relation Nth time if any distance is changed that mean there is negative cycle
-		// boolean flag = true;
-		// for(int j=0; j<m; j++) {
-		// 	for(int[] e : edges) {
-		// 		int u = e[0], v = e[1], w = e[2];
-		// 		if((d[u] + w) < d[v]) {
-		// 			flag = false;
-		// 		}
-		// 	}
-		// }
-		
-		// if(flag == false)
-		// 	System.out.println(" Finding Shortest path is not possible as negative cycle exists");
-		// else// return distance vector 
-		// 	return d;
-        int ans = 0;
-        for(int i=0; i<d.length; i++) {
-            if(d[i] <= ds)
-                ans++;
-        }
-		return ans;
-	}
 
-    public static int dijkstra(int src, int n, int ds) {
-		int[] d = new int[2*n+1];
-		
-		//initialize dist with inf for every node from source
-		Arrays.fill(d, Integer.MAX_VALUE);
-		PriorityQueue<Node> pq = new PriorityQueue<>(new comp());
-		
-		//Insert src node with distance from src as 0 in pq
-		pq.add(new Node(src, 0));
-		d[src] = 0;
-		
-		while(!pq.isEmpty()) {
-			Node node = pq.poll();
-			
-			int curr = node.node;
-			int distance = node.dist;
-			
-			if(d[curr] != distance)
-                continue;
-			for(Edge e : adj.get(curr)) {
-				int nb = e.node;
-				int w = e.weight;
-				
-				if(d[nb] > d[curr] + w) {
-					d[nb] = d[curr] + w;
-					pq.add(new Node(nb, d[nb]));
-				}
-			}
-		}
-        int ans = 0;
-        
-        for(int i=0; i<d.length; i++) {
-            //System.out.print(d[i]+" ");
-            if(d[i] <= ds)
-                ans++;
-        }
-        //System.out.println("src= "+src+"- ans = "+ans);
-		return ans;
-	}
+		// Get done with the phases
+		for(int k = 0; k < n; ++k) // kth phase
+            for(int i = 0; i < n; ++i)
+                for(int j = 0; j < n; ++j)
+                    d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
+
+		return d;
+    }
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[] cities = new int[n];
-        int[][] e = new int[2*edges.length][3];
-        int k = 0;
-        adj = new ArrayList<>();
-        for(int i = 0; i <= n; ++i)
-            adj.add(new ArrayList<>());
-        for(int i=0; i<edges.length; i++) {
-            int u = (edges[i][0]);
-            int v = (edges[i][1]);
-            int wt = (edges[i][2]);
+        //Solve using floyd warshell
 
-            adj.get(u).add(new Edge(v, wt));
-            adj.get(v).add(new Edge(u, wt));
-
-            e[k][0] = edges[i][0];
-            e[k][1] = edges[i][1];
-            e[k++][2] = edges[i][2];
-
-            e[k][0] = edges[i][1];
-            e[k][1] = edges[i][0];
-            e[k++][2] = edges[i][2];
-        }
-        //BellMan Ford
-        // for(int src=0; src<n; src++)
-        //     cities[src] = bellmanFord(src, distanceThreshold, e, n);
+        int[][] d = floydWarshell(edges, n);
         
-        //Dijkastra
-        for(int src=0; src<n; src++)
-            cities[src] = dijkstra(src, n, distanceThreshold);
+        int ans = -1, minCount = Integer.MAX_VALUE;
+		for(int i = 0; i < n; ++i) {
+			int curCount = 0;
+			for(int j = 0; j < n; ++j)
+				if(d[i][j] <= distanceThreshold)
+					curCount += 1;
+            System.out.println(curCount);
+			if(curCount <= minCount) {
+				minCount = curCount;
+				ans = i;
+			}
+		}
 
-        int result = Integer.MAX_VALUE;
-        int city = n-1;
+		return ans;
 
-        for(int src=0; src<n; src++) {
-            //System.out.print(cities[src]+"   ");
-            if(result >= cities[src]) {
-                result = cities[src];
-                city = src;
-            }
-        }
-        return city;
     }
 }
