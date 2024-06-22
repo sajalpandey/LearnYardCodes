87912c935d1737bc83ec19e5ac87559d5d581101
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-06-22 20:59:11 +0530

    Time: 44 ms (7.85%) | Memory: 55.1 MB (36.41%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -4,7 +4,11 @@ class Solution {
         prefix.put(0,1);
         int count = 0, result = 0;
         for(int i=0; i<nums.length; i++) {
-            nums[i] = nums[i]%2==0 ? 0 : 1;            
+            if(nums[i]%2==0)
+                nums[i] = 0;
+            else
+                nums[i] = 1;
+            
             count += nums[i];
             int rem = count - k;
             if(prefix.containsKey(rem)) {
