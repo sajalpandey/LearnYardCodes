3e556fac0420649fee14cf241ede4dbc53a5b0bf
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-07-10 12:10:08 +0530

    Time: 17 ms (5.39%) | Memory: 43 MB (9.50%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        long result = 0;
+        StringBuilder str = new StringBuilder(s.trim());
+        if(str.length() == 0)
+            return 0;
+        int sign = 1;
+        int i = 0;
+        if(str.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if(str.charAt(i) == '+')
+            i++;
+        System.out.println("sign = "+sign+" -- i = "+i+" --str = ");
+        while(i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if(result * sign >= Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else if(result * sign <= Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+            result = result * 10 + (str.charAt(i) - '0');
+            i++;
+        }
+        //System.out.println("result = "+result);
+        System.out.println("result = "+result);
+        if(result * sign >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else if(result * sign <= Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        return (int)result*sign;
+    }
+}
