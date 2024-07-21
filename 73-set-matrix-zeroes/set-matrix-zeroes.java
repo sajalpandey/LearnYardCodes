86bcfdc93895251043130c645d91feaa3fc91223
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-07-21 15:10:16 +0530

    Time: 1 ms (81.11%) | Memory: 45.6 MB (21.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,32 +1,44 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        int row[] = new int[r];
-        int col[] = new int[c];
-        
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++) {
-                if(matrix[i][j]==0) {
-                    row[i] = 1;
-                    col[j] = 1;
+        boolean isFirstRowZero = false, isFirstColZero = false;
+        for(int i=0; i<matrix.length; i++) {
+            if(matrix[i][0] == 0)
+                isFirstColZero = true;
+        }
+
+        for(int i=0; i<matrix[0].length; i++) {
+            if(matrix[0][i] == 0)
+                isFirstRowZero = true;
+        }
+        //one pass to mark first row and col as '0' if row & col contains any zero
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[i].length; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        
-        for(int i=0; i<r; i++){
-            if(row[i]==1){
-                for(int j=0; j<c; j++)
+
+        //now make them zero
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[i].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
             }
         }
-        
-        for(int j=0; j<c; j++){
-            if(col[j]==1){
-                for(int i=0; i<r; i++)
-                    matrix[i][j] = 0;
+
+        //mark first row & col as zero
+        if(isFirstColZero == true) {
+            for(int i=0; i<matrix.length; i++) {
+                matrix[i][0] = 0;
             }
         }
-        
+
+        if(isFirstRowZero == true) {
+            for(int i=0; i<matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
 }
