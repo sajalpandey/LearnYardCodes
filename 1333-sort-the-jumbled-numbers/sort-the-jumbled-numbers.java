929b760cb6ddffcdadd88eff91a5d5018ee984ff
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-07-24 15:43:03 +0530

    Time: 85 ms (85.29%) | Memory: 55.5 MB (48.04%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -8,26 +8,18 @@ class Solution {
                 new_num = mapping[0];
             else {
                 while(number > 0) {
-                    int digit = number%10;
-                    //System.out.println("digit = "+digit+" --mapping[digit] "+mapping[digit]);
-
-                    new_num = new_num + mapping[digit] * mul;
+                    new_num = new_num + mapping[number%10] * mul;
                     mul *= 10;
-                    //System.out.println("new_num = "+new_num);
                     number /= 10;
                 }
             }
-            
-            //System.out.println(i+" -- "+new_num.reverse());
             list.add(new int[] { i,  new_num});
-            //list.add(new int[] {i, Integer.parseInt(new_num.toString)});
         }
         Collections.sort(list, (a, b) -> ((a[1] != b[1] ? a[1] - b[1] : a[0] - b[0])));
 
         int[] result = new int[nums.length];
         int k = 0;
         for(int[] p : list) {
-            //System.out.println(p[0]+" "+p[1]);
             result[k++] = nums[p[0]];
         }
         return result;
