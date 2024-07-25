29a7a5db65ee1ca85c6b5cfaa209ee83211c509f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-07-25 10:33:31 +0530

    Time: 28 ms (50.43%) | Memory: 56.1 MB (53.26%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,30 +1,37 @@
 class Solution {
-    public static int[] merge(int[] left, int [] right) {
-        int n = left.length + right.length;
-        int [] result = new int[n];
-        int i=0,j=0,k=0;
-        while(i<left.length || j<right.length) {
-            if(j==right.length || i < left.length && left[i] <= right[j])
-                result[k++] = left[i++];
-            else
-                result[k++] = right[j++];
-        }
-        // while(i<left.length)
-        //     result[k++] = left[i++];
-        // while(j<right.length)
-        //     result[k++] = right[j++];
-        return result;
+    public void merge(int[] nums, int l, int mid, int r) {
+        int n1 = mid-l+1, n2 = r-mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
 
+        for(int i=0;i<n1;i++)
+            left[i] = nums[l+i];
+        for(int i=0; i<n2; i++)
+            right[i] = nums[mid+1+i];
+        
+        int i=0, j=0, k = l;
+        while(i < n1 && j < n2) {
+            if(left[i] <= right[j])
+                nums[k++] = left[i++];
+            else
+                nums[k++] = right[j++];
+        }
+        while(i < n1)
+            nums[k++] = left[i++];
+        while(j < n2)
+            nums[k++] = right[j++];
+    }
+    public void mergeSort(int[] nums, int l, int r) {
+        if(l >= r)
+            return;
+        int mid = (l+r)/2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid+1, r);
+
+        merge(nums, l, mid, r);
     }
     public int[] sortArray(int[] nums) {
-        int n = nums.length;
-        if(n==1)
-            return nums;
-        int [] left = Arrays.copyOfRange(nums,0,n/2);
-        int [] right = Arrays.copyOfRange(nums,n/2,n);
-
-        left = sortArray(left);
-        right = sortArray(right);
-        return merge(left,right);
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
     }
 }
