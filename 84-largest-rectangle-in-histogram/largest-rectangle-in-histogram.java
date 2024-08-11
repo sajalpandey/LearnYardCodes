593a03f56a0ed4942cf543aa32dd647974078780
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-08-11 23:18:29 +0530

    Time: 70 ms (57.53%) | Memory: 57.5 MB (38.57%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,44 +1,22 @@
 class Solution {
-    public int [] prevSmallerEle(int[] heights) {
-        int [] prevSmaller = new int[heights.length];
-        Stack<Integer> st = new Stack<>();
-        //st.push(-1);
-        for(int i=0; i<heights.length; ++i) {
-            while(!st.empty() && heights[st.peek()] >= heights[i])
-                st.pop();
-            prevSmaller[i] = st.empty() == true ? -1 : st.peek();
-            st.push(i);
-        }
-        return prevSmaller;
-    }
-
-    public int [] nextSmallerEle(int[] heights) {
-        int [] nextSmaller = new int[heights.length];
-        Stack<Integer> st = new Stack<>();
-        //st.push(heights.length);
-        for(int i=heights.length-1; i>=0; --i) {
-            while(!st.empty() && heights[st.peek()] >= heights[i])
-                st.pop();
-            nextSmaller[i] = st.empty() == true ? heights.length : st.peek();
-            st.push(i);
-        }
-        return nextSmaller;
-    }
-
     public int largestRectangleArea(int[] heights) {
-        //First find index of first smaller ele on left side
-        // find index for first smaller ele on right side
-        // now the area for that index will be height[j] * (r-l-1);
-        int n = heights.length;
-        int prevSmaller [] = new int[n];
-        int nextSmaller [] = new int[n];
-
-        prevSmaller = prevSmallerEle(heights);
-        nextSmaller = nextSmallerEle(heights);
-        int result = Integer.MIN_VALUE;
-        for(int i=0; i<n; ++i) {
-            result = Math.max(result, heights[i] * (nextSmaller[i] - prevSmaller[i] - 1));
+        Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
+        for(int i=0; i<heights.length; i++) {
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                int ele = st.peek();
+                st.pop();
+                int prevSmaller = st.isEmpty() ? -1 : st.peek();
+                maxArea = Math.max(maxArea, (i- prevSmaller - 1)*heights[ele]);
+            }
+            st.push(i);
         }
-        return result;
+
+        while(!st.isEmpty()) {
+            int ele = heights[st.pop()];
+            int prevSmaller = st.isEmpty() ? -1 : st.peek();
+            maxArea = Math.max(maxArea, (heights.length- prevSmaller - 1)*ele);
+        }
+        return maxArea;
     }
 }
