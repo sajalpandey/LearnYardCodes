bf7131b5e83d4c29d2c67522eec10092f21244ab
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-08-15 12:50:46 +0530

    Time: 9 ms (72.17%) | Memory: 42.8 MB (38.94%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -4,14 +4,13 @@ class Solution {
         int i = 0, j = 0, maxLen = 0, maxFreq = 0;
         while(j < s.length()) {
             char ch = s.charAt(j);
-            map[ch - 'A']++;
-            maxFreq = Math.max(maxFreq, map[ch - 'A']);
+            maxFreq = Math.max(maxFreq, ++map[ch - 'A']); 
             /**
             Changes = window length - maxFreq of char till now
             whene ever changes > k, will shrink our window
              */
             if((j-i+1) - maxFreq > k) {
-                map[s.charAt(i) - 'A']--;
+                --map[s.charAt(i) - 'A'];
                 i++;
             }
             j++;
