ffe899e00dde3423e989dc5b4d70e64225ff4cdd
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-08-15 11:10:38 +0530

    Time: 2 ms (95.27%) | Memory: 55.7 MB (74.15%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,35 +1,28 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int [] coins = new int [21];
-        for(int i=0; i<bills.length; i++) {
-            int rem = bills[i] - 5;
-            //System.out.println("bill--> "+bills[i]+" rem = "+rem+" -5Rs: "+coins[5]+" -10Rs: "+coins[10]+" -20Rs: "+coins[20]);
-            coins[bills[i]]++;
-
+        int five = 0, ten = 0;
+        for(int ele : bills) {
+            if(ele == 5)
+                five++;
+            else if(ele == 10)
+                ten++;
+            int rem = ele - 5;
             if(rem == 0)
                 continue;
-            //if(rem >= 20) {
-                while(rem -20 >= 0 && coins[20] > 0) {
-                    rem -= 20;
-                    coins[20]--;
-                }
-            //}
-           
-            //if(rem >= 10) {
-                while(rem -10 >= 0 && coins[10] > 0) {
+            else if(rem == 5 && five > 0)
+                five--;
+            else if(rem == 15) {
+                if(ten > 0) {
+                    ten--;
                     rem -= 10;
-                    coins[10]--;
                 }
-            //}
-
-            //if(rem >= 5) {
-                while(rem -5 >= 0 && coins[5] > 0) {
-                    rem -= 5;
-                    coins[5]--;
-                }
-            //}
-
-            if(rem!=0)
+                if(rem == 5 && five > 0)
+                    five--;
+                else if(rem == 15 && five >= 3)
+                    five -= 3;
+                else
+                    return false;
+            } else
                 return false;
         }
         return true;
