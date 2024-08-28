b8dc31a9d656d465a3833597bb484c8c0dd8cf10
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-08-28 10:37:53 +0530

    Time: 16 ms (61.25%) | Memory: 44.6 MB (5.87%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,13 +1,13 @@
 class Solution {
     public int countTriples(int n) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> map = new HashSet<>();
         for(int i=1; i<=n; i++)
-            map.put(i*i, i);
+            map.add(i*i);
         
         int count = 0;
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-                if(i < j && map.containsKey(i*i + j*j)) {
+                if(i < j && map.contains(i*i + j*j)) {
                     count++;
                 }
             }
