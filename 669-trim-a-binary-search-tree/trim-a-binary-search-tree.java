e5db38ac37a077164f72253d580b788f05ebdcc0
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-09-21 16:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (6.17%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.java
@@ -14,45 +14,37 @@
  * }
  */
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int val) {
-        System.out.println("val = "+val);
-        if (root == null) {
-            return root;
-        }
-
-        //if root is having only one child left OR right
-        if(root.left == null)
-            return root.right;
-        else if(root.right == null)
-            return root.left;
-        //if root is having both left AND right child
-        //then replace root with min from right sub tree;
-        root.val = minValue(root.right);
-        root.right = deleteNode(root.right, root.val);
+    public TreeNode findMax(TreeNode root) {
+        while(root.right != null)
+            root = root.right;
         return root;
     }
-    int minValue(TreeNode root) {
-        int minv = root.val;
-        while (root.left != null) {
-            minv = root.left.val;
-            root = root.left;
-        }
-        System.out.println("min value = "+minv);
-        return minv;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null)
+            return null;
+        if(root.val == key) {
+            if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+            TreeNode maxNode = findMax(root.left);
+            root.val = maxNode.val;
+            root.left = deleteNode(root.left, root.val);
+        }  
+        else if(root.val < key)
+            root.right = deleteNode(root.right, key);
+        else 
+            root.left = deleteNode(root.left, key);
+        return root;
     }
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if(root == null)
-            return null;
-        System.out.println("trimBST = "+root.val);
-        
-        if(root!= null && root.left != null)
-            root.left = trimBST(root.left, low, high);
-        if(root!= null && root.right != null)
-            root.right = trimBST(root.right, low, high);
-        if(root.val < low || root.val > high) {
+            return root;
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+
+        if(root.val < low || root.val > high)
             root = deleteNode(root, root.val);
-            root = trimBST(root, low, high);
-        }
         return root;
     }
 }
