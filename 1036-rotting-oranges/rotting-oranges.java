24bc883c28a5bd732093a2afdd99730453c344e3
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-01 22:51:44 +0530

    Time: 2 ms (71.72%) | Memory: 42.5 MB (39.12%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,65 +1,46 @@
-class Pair {
-    int row;
-    int col;
-    int time;
-    Pair(int i, int j, int t) {
-        this.row = i;
-        this.col = j;
-        this.time = t;
-    }
-}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        //This is MULTI SOURCE BFS QUESTION
-        int m = grid.length;
-        int n =grid[0].length;
-
-        Queue<Pair> qq = new LinkedList<>();
+        int m = grid.length; int n = grid[0].length;
         int[][] vis = new int[m][n];
+        int countFresh = 0;
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                //Add all rotten orange as source of BFS with co-ordinates and time as 0
+                // if its rotten add it in queue
                 if(grid[i][j] == 2) {
-                    qq.add(new Pair(i,j,0));
-                    //mark visited
+                    q.add(new int[] {i, j});
                     vis[i][j] = 2;
-                } else if(grid[i][j] == 1)
-                    vis[i][j] = 1;
+                } else if(grid[i][j] == 1) 
+                    countFresh++;
             }
         }
-
-        //Now start BFS
+        
         int ans = 0;
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
-        while(!qq.isEmpty()) {
-            Pair curr = qq.poll();
-            int r = curr.row;
-            int c = curr.col;
-            int t = curr.time;
-            ans = Math.max(ans, t);
-
-            //check in all four directions
-            for(int i=0; i<4; i++) {
-                int p = r + drow[i];
-                int q = c + dcol[i];
-                // new bounds should be with in range and these
-                //cells should not be visited and oranges should be fresh on these cells
-                if(p >=0 && p < m && q >= 0 && q < n) {
-                    if(vis[p][q] != 2 && grid[p][q] == 1) {
-                        qq.add(new Pair(p, q, t+1));
-                        vis[p][q] = 2;
+        //System.out.println("Sajal = "+countFresh);
+        if(countFresh == 0)
+            return 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            ans++;
+            while(size-- > 0) {
+                int[] row = q.poll();
+                for(int i=0; i<4; i++) {
+                    int newR = row[0] + drow[i];
+                    int newC = row[1] + dcol[i];
+                    //if new bounds are within 
+                    if(newR >=0 && newR < m && newC >=0 && newC < n) {
+                        if(vis[newR][newC]!=2 && grid[newR][newC] == 1) {
+                            vis[newR][newC] = 2;
+                            q.add(new int[] {newR, newC});
+                            countFresh--;
+                        }
                     }
                 }
             }
         }
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++) {
-                if(vis[i][j] == 1)
-                    return -1;
-            }
-        }
-        return ans;
+        //System.out.println("Sajal2 = "+countFresh);
+        return (countFresh == 0 ? ans -1 : -1 );
     }
 }
