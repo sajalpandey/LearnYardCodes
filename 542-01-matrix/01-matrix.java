30a7729b501c6e01f8169bace63f614113c94309
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-02 19:35:54 +0530

    Time: 14 ms (65.93%) | Memory: 47.4 MB (43.59%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,61 +1,42 @@
-class Node {
-    int x;
-    int y;
-    int step;
-    Node(int x, int y, int s) {
-        this.x = x;
-        this.y = y;
-        this.step = s;
-    }
-}
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        //Idea is ti find distance of nearest 0 from each 0
+        //Idea is to find distance of nearest 0 from each 0
         //using BFS
         int m = mat.length;
         int n = mat[0].length;
-        Queue<Node> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         int[][] d = new int[m][n];
         boolean[][] vis = new boolean[m][n];
         for(boolean[] row : vis)
             Arrays.fill(row, false);
         //For each node with value "0" the dist to nearest 0 to be zero 
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
                 if(mat[i][j] == 0) {
-                    q.add(new Node(i, j, 0));
+                    q.add(new int[] {i, j, 0});
                     vis[i][j] = true;
                     d[i][j] = 0;
                 }
+            }
+        }
+        int[] drow = {-1,1,0,0};
+        int[] dcol = {0,0,-1,1};
+
         while(!q.isEmpty()) {
-            Node temp = q.poll();
-            int x = temp.x;
-            int y = temp.y;
-            int steps = temp.step;
-            d[x][y] = steps;
-            //check for each 4 neighbour 
-            //up
-            if((x-1) >=0 && vis[x-1][y] == false) {
-                vis[x-1][y] = true;
-                q.add(new Node(x-1, y, steps+1));
-            }
+            int[] row = q.poll();
+            int p = row[0];
+            int s = row[1];
+            int dist = row[2];
 
-            //down
-            if((x+1) < m && vis[x+1][y] == false) {
-                vis[x+1][y] = true;
-                q.add(new Node(x+1, y, steps+1));
-            }
-
-            //left
-            if((y-1) >= 0 && vis[x][y-1] == false) {
-                vis[x][y-1] = true;
-                q.add(new Node(x, y-1, steps+1));
-            }
-
-            //right
-            if((y+1) < n && vis[x][y+1] == false) {
-                vis[x][y+1] = true;
-                q.add(new Node(x, y+1, steps+1));
+            //mark all 4 directions
+            for(int i=0; i<4; i++) {
+                int newR = p + drow[i];
+                int newC = s + dcol[i];
+                if(newR<0 || newR>m-1 || newC<0 || newC>n-1 || vis[newR][newC] == true)
+                    continue;
+                vis[newR][newC] = true;
+                d[newR][newC] = 1 + d[p][s];
+                q.add(new int[] {newR, newC, d[newR][newC]});
             }
         }
         return d;
