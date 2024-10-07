380b42328b318bcaccbcd317885273d917dd6e58
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-07 22:02:07 +0530

    Time: 17 ms (53.97%) | Memory: 45.1 MB (84.76%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid[0][0] != 0)
+            return -1;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0, 1});
+
+        int m = grid.length, n = grid[0].length;
+        int[][] dist = new int[m][n];
+        for(int[] row : dist)
+            Arrays.fill(row, (int)1e9);
+        dist[0][0] = 1;
+        while(!q.isEmpty()) {
+            int[] tuple = q.poll();
+            int steps = tuple[2];
+            int x = tuple[0];
+            int y = tuple[1];
+            if(x == m-1 && y == n-1)
+                return steps;
+            for(int i=0; i<8; i++) {
+                int p = directions[i][0];
+                int qq = directions[i][1];
+                int newX = x + p;
+                int newY = y + qq;
+
+                if(newX<0 || newX>=m || newY<0 || newY>=n || grid[newX][newY] == 1)
+                    continue;
+                if(steps+1 < dist[newX][newY]) {
+                    dist[newX][newY] = steps+1;
+                    q.add(new int[]{newX, newY, steps+1});
+
+                }
+            }
+        }
+        return -1;
+    }
+}
