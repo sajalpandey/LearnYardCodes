7a4214f9496bd3dec6896fd9013ca09a937f7ddc
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-17 23:07:17 +0530

    Time: 1 ms (67.20%) | Memory: 46.3 MB (48.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,20 +1,26 @@
 class Solution {
     public int trap(int[] height) {
+        /**
+        We will try to find for each tower how much water it can contribute to the
+        total trap 
+        water contributed = Min(left max - rightMax) - tower height (for each tower)
+         */
+
+        //2 pass solution
         int n = height.length;
-        int result = 0;
-        int i = 0, j = n-1,lmax = 0, rmax = 0;
-        while(i <= j) {
-            if(height[i] <= height[j]) {
-                // fill from left side;
-                lmax = Math.max(lmax, height[i]);
-                result += lmax - height[i];
-                i++;
-            } else {
-                rmax = Math.max(rmax, height[j]);
-                result += rmax - height[j];
-                j--;
-            }
-        }
-        return result;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++)
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--)
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        
+        int totalWater = 0;
+        for(int i=0; i<n; i++)
+            totalWater += Math.min(leftMax[i], rightMax[i]) -  height[i];
+
+        return totalWater;
     }
 }
