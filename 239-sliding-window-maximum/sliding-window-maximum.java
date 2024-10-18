1dd03cdf18bb3e02cb464ca9a2b5b96605715823
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-18 19:27:26 +0530

    Time: 31 ms (65.48%) | Memory: 58.2 MB (88.47%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,23 +1,27 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int n = arr.length;
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //First approach is check every window of size k TC(n*k)
+        //Optimized is using deque
         Deque<Integer> dq = new LinkedList<>();
-        int result[] = new int[n-k+1];
-        int p=0;
-        for(int i=0; i<n; ++i) {
-            
-            while(dq.size() > 0 && arr[dq.peekLast()] <= arr[i])
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+        int p = 0, i = 0;
+        while(i < n) {
+            //Logic how to keep in Dq
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
                 dq.removeLast();
-            
-            dq.addLast(i);
-            
-            if(dq.peekFirst() <= i-k) {
+
+            //Logic when to remove from dq
+            while(!dq.isEmpty() && dq.peekFirst() <= (i-k))
                 dq.removeFirst();
-            }
-            
+
+            dq.addLast(i);
+            //update result
             if(i >= k-1)
-                result[p++] = arr[dq.peekFirst()];
+                result[p++] = nums[dq.peekFirst()];
+            i++;
         }
+
         return result;
     }
 }
