2021d364153441201b7c34169bdb66e67336fbe3
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-23 19:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (14.17%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,25 +1,31 @@
 class Solution {
     public int findMin(int[] nums) {
-        int n = nums.length - 1;
-        int start = 0, end = nums.length - 1;
-        if(nums.length==2)
-            return Math.min(nums[start],nums[end]);
-        if(nums.length==1)
+        /**
+        [4,5,6,7,0,1,2]
+        [6,7,0,1,2,4,5]
+         */
+        int n = nums.length;
+        if(n == 1)
             return nums[0];
-        if(nums[0] < nums[n])
+        if(n == 2) 
+            return Math.min(nums[0], nums[1]);
+        
+        // we have sorted array , but not rotated
+        if(nums[0] < nums[n-1])
             return nums[0];
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            if(mid == n && nums[mid] < nums[mid-1]) {
+        
+        int low = 0, high = n-1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(mid == n-1 || (nums[mid] < nums[mid+1]  && nums[mid] < nums[mid-1]))
                 return nums[mid];
-            }
-            if((mid-1) >= 0 && nums[mid] < nums[mid-1] && (mid+1) <= n &&  nums[mid] < nums[mid+1]) {
-                return nums[mid];
-            } else if (nums[mid] < nums[end]) 
-                end = mid - 1;
-            else 
-                start = mid + 1;
+            
+            if(nums[mid] < nums[high])
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         return -1;
+        
     }
 }
