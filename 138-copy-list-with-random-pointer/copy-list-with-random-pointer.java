78928237162e21abf840b913f04a33a7844344c0
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-27 19:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (70.47%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,28 +15,57 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        /**
+        7 --> 13 --> 11 --> 10 --> 1
+                     curr             curr           curr            curr
+        7 --> (7) --> 13 --> (13) --> 11 --> (11) --> 10 --> (10) --> 1 --> (1) --> null
+        */
+        // original node = curr
+        // mera node = curr.next
+
+        // curr.next.random = curr.random.next
+        // original_node.random
+
+        //1. insert copy nodes in between
+        if(head == null)
+            return null;
         Node curr = head;
-        while(curr!=null) {
+        while(curr != null) {
             Node newNode = new Node(curr.val);
-            newNode.next = curr.next;
+            Node temp = curr.next;
             curr.next = newNode;
-            curr = newNode.next;
+            newNode.next = temp;
+
+            curr = temp;
         }
         curr = head;
-        while(curr!=null) {
-            if(curr.random!=null)
+        
+        //2. update our nodes random pointers
+
+        while(curr != null) {
+
+            if(curr.random != null) {
                 curr.next.random = curr.random.next;
+            }
+
             curr = curr.next.next;
         }
+
+        //separte our list from original one
         Node dummy = new Node(-1);
-        Node ptr = dummy;
+        dummy.next = head.next;
+
         curr = head;
-        while(curr!=null) {
-            ptr.next = curr.next;
-            ptr = ptr.next;
+        Node ptr = curr.next;
+        while(curr != null) {
             curr.next = ptr.next;
             curr = curr.next;
+            if(curr == null)
+                break;
+            ptr.next = curr.next;
+            ptr = ptr.next;
         }
+
         return dummy.next;
     }
 }
