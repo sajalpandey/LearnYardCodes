0c1c7e814bd0df79ead18895b22df4dd078f6463
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-10-30 22:09:21 +0530

    Time: 9 ms (98.28%) | Memory: 44.7 MB (97.11%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,10 +1,11 @@
 class Solution {
-    public boolean check(int[] hand, int groupSize, int i) {
+    public boolean checkGroupPossible(int[] hand, int i, int groupSize) {
         int ele = hand[i];
-        hand[i++] = -1;
+        hand[i] = -1;
+        i++;
         int count = 1;
         while(i < hand.length && count < groupSize) {
-            if(hand[i] == ele+1) {
+            if((1 + ele) == hand[i]) {
                 ele = hand[i];
                 hand[i] = -1;
                 count++;
@@ -15,17 +16,19 @@ class Solution {
         if(count == groupSize)
             return true;
         return false;
+
     }
     public boolean isNStraightHand(int[] hand, int groupSize) {
         int n = hand.length;
         if(n % groupSize != 0)
             return false;
+        
         Arrays.sort(hand);
-
         for(int i=0; i<n; i++) {
-            if(hand[i] >= 0)
-                if(!check(hand, groupSize, i))
+            if(hand[i] >= 0) {
+                if(!checkGroupPossible(hand, i, groupSize)) 
                     return false;
+            }
         }
         return true;
     }
