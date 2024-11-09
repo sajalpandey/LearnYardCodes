9b423569af9706d50b807c540421b38b77d09b51
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-09 15:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (89.89%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,43 +1,28 @@
 class Solution {
-    public static void helper(List<String> result, StringBuilder str, int open, int close, int n) {
-        if(close > open || open > n || close > n)
+    public void helper(int open, int close, int n, StringBuilder str, List<String> result) {
+        if(open > n || close > n || close > open)
             return;
-
-        if(open + close == 2* n) {
+        if(str.length() == 2*n) {
             result.add(str.toString());
             return;
         }
 
-        if(open < n) {
+        if(open <  n) {
             str.append('(');
-            helper(result, str, open+1, close, n);
+            helper(open+1, close, n , str , result);
+            str.deleteCharAt(str.length()-1);
+        }
+        if(close <= open) {
+            str.append(')');
+            helper(open, close+1, n , str , result);
             str.deleteCharAt(str.length()-1);
         } 
         
-        if(close <= open) {
-            str.append(')');
-            helper(result, str, open, close+1, n);
-            str.deleteCharAt(str.length()-1);
-        }
-
+        return;
     }
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        StringBuilder str = new StringBuilder();
-        helper(result, str,0,0,n);
+        helper(0, 0, n, new StringBuilder(), result);
         return result;
-        /**
-        n = 1
-        ()
-        open_b < Close
-        always open >= close --> open == close
-        n = 2
-        ()() , (())
-        Close > open -- return
-        if open + close == 2 * n
-        ans array me append
-        n = 3
-        )   )()(), (( ))(), ( ) ( ( ) ), (()()), ((()))
-         */
     }
 }
