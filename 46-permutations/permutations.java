ca64a9717bc02b56980f1e73d2139405cf8a667c
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-09 15:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (33.68%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,31 +1,29 @@
 class Solution {
-    public static void swap(int[] array, int index1, int index2) {
-        // Temporary storage of the first element
-        int temp = array[index1];
-        // Assign the second element to the first element's position
-        array[index1] = array[index2];
-        // Assign the stored first element to the second element's position
-        array[index2] = temp;
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
     }
-    public static void helper(List<List<Integer>> result,int [] nums, int i) {
-        if(i == nums.length) {
-            List<Integer> perm = new ArrayList<>();
+    public void permuteHelper(int[] nums, int index, List<List<Integer>> result) {
+        if(index == nums.length) {
+            List<Integer> tempList = new ArrayList<>();
             for (int num : nums) {
-                perm.add(num);
+                tempList.add(num);
             }
-            result.add(perm);
+            result.add(tempList);
             return;
         }
-        
-        for(int j=i; j<nums.length; j++) {
-            swap(nums,i,j);
-            helper(result,nums,i+1);
-            swap(nums,i,j);
+        //now start to swap 
+        for(int i=index; i<nums.length; i++) {
+            swap(nums, index, i);
+            permuteHelper(nums, index+1, result);
+            swap(nums, index, i);
         }
+        return;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        helper(result,nums,0);
+        permuteHelper(nums, 0, result);
         return result;
     }
 }
