146a377e40052942f9246e628f4195e5b18202c0
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-10 07:31:31 +0530

    Time: 4 ms (48.69%) | Memory: 43.4 MB (18.45%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,28 @@
 class Solution {
-    public static void combinationSum2Helper(List<List<Integer>> result, int[] candidates, List<Integer> temp, int i, int target) {
+    public void combinationSum2Helper(int[] candidates, int index, List<Integer> temp,
+    int target, List<List<Integer>> result) {
+        //base case
+        if(target < 0 || index > candidates.length)
+            return;
+        // if target becomes 0
         if(target == 0) {
-            result.add(new ArrayList<Integer>(temp));
+            result.add(new ArrayList<>(temp));
             return;
         }
-        if(target < 0 || i >= candidates.length)
-            return;
-
-        temp.add(candidates[i]);
-        combinationSum2Helper(result, candidates, temp, i+1, target-candidates[i]);
-        //backtrack
-        temp.remove(temp.size()-1);
-        // Skip duplicates
-        while (i + 1 < candidates.length && candidates[i] == candidates[i + 1]) {
-            i++;
+        for(int i=index; i<candidates.length; i++) {
+            //avoid duplicates
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            temp.add(candidates[i]);
+            combinationSum2Helper(candidates, i+1, temp, target-candidates[i], result);
+            temp.remove(temp.size()-1);
+            //combinationSum2Helper(candidates, i+1, temp, target, result);
         }
-        combinationSum2Helper(result, candidates, temp, i+1, target);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        //System.out.println(Arrays.toString(candidates));
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        combinationSum2Helper(result, candidates, temp, 0, target);
+        combinationSum2Helper(candidates, 0, new ArrayList<>(), target, result);
         return result;
     }
 }
