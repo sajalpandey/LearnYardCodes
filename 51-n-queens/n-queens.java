99c2d676b077a566bf315a314b11311ad9ee479d
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-10 16:32:15 +0530

    Time: 2 ms (87.22%) | Memory: 44.8 MB (60.75%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,39 +1,40 @@
 class Solution {
-    public static void solveNQueensHelper(List<List<String>> result,List<String> board,int row,boolean[] top, boolean[] topLeft,boolean[] topRight, int n) {
+    public void solveNQueensHelper(int row, int n, List<List<String>> result, List<String> board,
+    boolean[] top, boolean[] topleft, boolean[] topright) {
+        // we have placed n queens
         if(row == n) {
-            result.add(new ArrayList<String>(board));
+            result.add(new ArrayList<>(board));
             return;
         }
-        //iterate over every col for given ROW , try to place queen
-        for(int j=0; j<n; ++j) {
-            if(!top[j] && !topLeft[row-j+n-1] && !topRight[row+j]) {
-                top[j] = true;
-                topLeft[row-j+n-1] = true;
-                topRight[row+j] = true;
-                char[] r = new char[n];
-                Arrays.fill(r,'.');
-                r[j] = 'Q';
-                board.add(new String(r));
-                // Placed a queen in jth column of row and as recursion to place queens in next row
-                solveNQueensHelper(result, board, row+1, top, topLeft, topRight, n);
 
-                // Now backtrack to find another combination
+        //now we will try to place Queen in given row, try every where in column
+        for(int i=0; i<n; i++) {
+            //we can place a queen if all 3 array allows
+            if(!top[i] && !topleft[row-i+n-1] && !topright[row+i]) {
+
+                top[i] = true;
+                topright[row+i] = true;
+                topleft[row-i+n-1] = true;
+                char[] temp = new char[n];
+                Arrays.fill(temp, '.');
+                temp[i] = 'Q';
+                board.add(new String(temp));
+                solveNQueensHelper(row+1, n, result, board, top, topleft, topright);
+
+                top[i] = false;
+                topright[row+i] = false;
+                topleft[row-i+n-1] = false;
                 board.remove(board.size()-1);
-                top[j] = false;
-                topLeft[row-j+n-1] = false;
-                topRight[row+j] = false;
             }
         }
-    }
-
+    } 
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
         List<String> board = new ArrayList<>();
         boolean[] top = new boolean[n];
-        boolean[] topLeft = new boolean[2*n];
-        boolean[] topRight = new boolean[2*n];
-
-        solveNQueensHelper(result,board,0,top,topLeft,topRight,n);
+        boolean[] topleft = new boolean[2*n];
+        boolean[] topright = new boolean[2*n];
+        solveNQueensHelper(0, n, result, board, top, topleft, topright);
         return result;
     }
 }
