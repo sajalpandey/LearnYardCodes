dc2dd647a7a38d367037a612cea65ed4b8bdbad5
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-12 08:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (54.68%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -13,12 +13,32 @@ class Solution {
         return dp[n] = topDownHelper(n-1, dp) + topDownHelper(n-2, dp);
     }
     public int climbStairs(int n) {
-        //1. Recursion|| TC: O(n) || SC:O(n)
+        //1. Recursion|| TC: O(2^n) || SC:O(n) 
         //return helper(n);
 
-        //2.Memoization(Top Down)
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return topDownHelper(n, dp);
+        //2.Memoization(Top Down)|| TC: O(n) || SC:O(n)
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return topDownHelper(n, dp);
+
+        //3. Bottom Up(Iterative)|| TC: O(n) || SC:O(n)
+        // if(n <= 2)
+        //     return n;
+        // int[] dp = new int[n+1];
+        // dp[1] = 1; dp[2] = 2;
+        // for(int i=3; i<=n; i++)
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // return dp[n];
+
+        //4. Space optimized
+        if(n <= 2)
+            return n;
+        int prev2 = 1, prev = 2, curr = 0;
+        for(int i=3; i<=n; i++) {
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
     }
 }
