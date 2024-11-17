69c258d381506ca6637f9b9f3cbbcdf3a093ab90
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-17 12:28:21 +0530

    Time: 1 ms (80.30%) | Memory: 41.8 MB (51.45%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,70 +1,68 @@
 class Solution {
     public boolean isValid(String str) {
+        boolean result = false;
         if(str.length() == 1) {
-            return (str.charAt(0) == '0' ? false : true);
-        } else if(str.length() == 2) {
-            if(str.charAt(0) == '0')
-                return false;
+            result = ((str.charAt(0) == '0') ? false : true);
+            //System.out.println(str+" -- "+result);
+        }
+        if(str.charAt(0) != '0') {
             int num = Integer.parseInt(str);
             if(num >= 10 && num <= 26)
-                return  true;
+                result = true;
         }
-        return false;
+        //System.out.println(str+" -- "+result);
+        return result;
     }
-    public int numDecodingsHelper(String s, int i, int[] dp) {
-        if(i >= s.length()) {
+    public int numDecodingsHelper(String s, int i) {
+        //this means we are able to partiton, reached till end
+        if(i >= s.length())
             return 1;
+        
+        //we have two choices either take 1 character or 2 character
+        int count1 = 0;
+        String str1 = s.substring(i, i+1);
+        if(isValid(str1))
+            count1 += numDecodingsHelper(s, i+1);
+        
+        int count2 = 0;
+        if(i+2 <= s.length()) {
+            String str2 = s.substring(i, i+2);
+            if(isValid(str2))
+                count2 += numDecodingsHelper(s, i+2);
         }
+
+        return count1+count2;
+    }
+    public int numDecodingsMemo(String s, int i, int[] dp) {
+        //this means we are able to partiton, reached till end
+        if(i >= s.length())
+            return 1;
         if(dp[i] != -1)
             return dp[i];
+        
+        //we have two choices either take 1 character or 2 character
         int count1 = 0;
-
-        //either take 1 char
-        String strLen1 = s.substring(i,i+1);
-        boolean flag = isValid(strLen1);
-        if(flag == true) {
-            count1 += numDecodingsHelper(s, i+1, dp);
-        }
-
-        //eight take 2 char
+        String str1 = s.substring(i, i+1);
+        if(isValid(str1))
+            count1 += numDecodingsMemo(s, i+1, dp);
+        
         int count2 = 0;
-        if((i+2) <= s.length()) {
-            String strLen2 = s.substring(i,i+2);
-            if(isValid(strLen2))
-                count2 += numDecodingsHelper(s, i+2, dp);
+        if(i+2 <= s.length()) {
+            String str2 = s.substring(i, i+2);
+            if(isValid(str2))
+                count2 += numDecodingsMemo(s, i+2, dp);
         }
-        dp[i] = count1+count2;
-        return dp[i];    
+
+        return dp[i] = count1+count2;
     }
     public int numDecodings(String s) {
+        //1. Recursive approach
         int n = s.length();
+        //return numDecodingsHelper(s, 0);
+
+        //2. Top Down
         int[] dp = new int[n+1];
-        dp[0] = 1;
         Arrays.fill(dp, -1);
-        /** SOL - 1 */
-        /** TOP DOWN APPROACH */
-        //return numDecodingsHelper(s, 0, dp);
-
-        /** SOL - 2 */
-        /** BOTTOM UP APPROACH */
-        Arrays.fill(dp, 0);
-        dp[n] = 1; // way to take a char of string length 1
-        for(int i=n-1; i>=0; i--) {
-            //take 1 char 
-            String strLen1 = s.substring(i,i+1);
-            if(isValid(strLen1)) {
-                dp[i] += dp[i+1]; // Add ways to decode i+1th char
-            }
-
-            //take 2 char
-            if((i+2) <= n) {
-                String strLen2 = s.substring(i,i+2);
-                if(isValid(strLen2))
-                    dp[i] += dp[i+2]; //Add ways to decode i+2th char
-            }
-        }
-
-        //return total number of ways
-        return dp[0];        
+        return numDecodingsMemo(s, 0 , dp);
     }
 }
