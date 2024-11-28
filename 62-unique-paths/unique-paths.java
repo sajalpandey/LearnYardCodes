06d7913e5a64d43cd1475452d549b192aae4eac2
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-28 20:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (28.53%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -27,9 +27,28 @@ class Solution {
         // return uniquePathsHelper(0, 0, m, n);
 
         int[][] dp = new int[m+1][n+1];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        // for(int[] row : dp)
+        //     Arrays.fill(row, -1);
         
-        return uniquePathsTopDown(0, 0, m, n, dp);
+        // return uniquePathsTopDown(0, 0, m, n, dp);
+
+        //Bottom up
+        //base case
+        //i--> 0 to m-1
+        //j--> 0 to n-1
+        for (int i = 0; i < m; i++) {
+            dp[i][n - 1] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[m - 1][j] = 1;
+        }
+        for(int i=m-2; i>=0; i--) {
+            for(int j=n-2; j>=0; j--) {
+                int down = dp[i+1][j];
+                int right = dp[i][j+1];
+                dp[i][j] = down+right;
+            }
+        }
+        return dp[0][0];
     }
 }
