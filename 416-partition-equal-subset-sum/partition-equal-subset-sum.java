1634f6bbec4a121623983a9777e6e965bf5c6dbd
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-11-28 18:48:43 +0530

    Time: 34 ms (67.65%) | Memory: 57.5 MB (21.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -44,9 +44,30 @@ class Solution {
         For dp we can thing like searching a subset of sum/2
          */
         int[][] dp = new int[n+1][sum/2+1];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        // for(int[] row : dp)
+        //     Arrays.fill(row, -1);
         
-        return canPartitionTopDown(nums, 0, sum/2, dp) == 1 ? true : false;
+        // return canPartitionTopDown(nums, 0, sum/2, dp) == 1 ? true : false;
+
+        //Now changing it to Bottom up
+        //base case , when target is 0 dp[i][target] = 1
+        //changing parameters
+        // i --> 0 to n-1
+        //target --> sum/2 to 0
+        //reverse them
+        for(int i=n; i>=0; i--) {
+            dp[i][0] = 1; 
+        }
+        for(int i=n-1; i>=0; i--) {
+            for(int target=0; target<=sum/2; target++) {
+                //copy the recusion calls
+                int op1 = dp[i+1][target];
+                int op2 = 0;
+                if(target-nums[i] >= 0)
+                    op2 = dp[i+1][target-nums[i]];
+                dp[i][target] = (op1 + op2 > 0 ? 1 : 0);
+            }
+        }
+        return dp[0][sum/2]==1 ? true: false;
     }
 }
