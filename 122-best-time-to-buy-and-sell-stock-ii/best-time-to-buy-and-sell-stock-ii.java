491454bd1c17582bbd4433e8567dacd3222e14e7
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2024-12-02 20:27:57 +0530

    Time: 3 ms (10.69%) | Memory: 49 MB (5.26%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,77 +1,28 @@
 class Solution {
-    public int maxProfitHelper(int[] prices, int i, int buy) {
+    public int helper(int[] prices, int i, int prevBought, int[][] dp) {
         if(i == prices.length)
             return 0;
-        
+        if(dp[i][prevBought] != -1)
+            return dp[i][prevBought];
+        //if stock is bought previously, then either sell today or not
+        //if stock is not bought then 2 choice buy it today or not
         int profit = 0;
-        if(buy == 1) {
-            //Its a buy call , we can buy or skip
-            int buyThis = (-prices[i] + maxProfitHelper(prices, i+1, 0));
-            int skipThis = (0 + maxProfitHelper(prices, i+1, 1));
-            profit = Math.max(buyThis, skipThis);
+        if(prevBought == 1) {
+            int op1 = (-prices[i] + helper(prices, i+1, 0, dp));
+            int op2 = 0 + helper(prices, i+1, 1, dp);
+            profit = Math.max(op1, op2);
         } else {
-            //Its a sell call, we can sell of skip 
-            int sellThis = (+prices[i] + maxProfitHelper(prices, i+1, 1));
-            int skipThis = (0 + maxProfitHelper(prices, i+1, 0));
-            profit = Math.max(sellThis, skipThis);
+            //sell call
+           int op1 = prices[i] + helper(prices, i+1, 1, dp);
+           int op2 = 0 + helper(prices, i+1, 0, dp);
+           profit = Math.max(op1, op2);
         }
-        return profit;
+        return dp[i][prevBought] = profit;
     }
-
-    public int maxProfitTopDown(int[] prices, int i, int buy, int[][] dp) {
-        if(i == prices.length)
-            return 0;
-        
-        if(dp[i][buy] != -1)
-            return dp[i][buy];
-
-        int profit = 0;
-        if(buy == 1) {
-            //Its a buy call , we can buy or skip
-            int buyThis = (-prices[i] + maxProfitTopDown(prices, i+1, 0, dp));
-            int skipThis = (0 + maxProfitTopDown(prices, i+1, 1, dp));
-            profit = Math.max(buyThis, skipThis);
-        } else {
-            //Its a sell call, we can sell of skip 
-            int sellThis = (+prices[i] + maxProfitTopDown(prices, i+1, 1, dp));
-            int skipThis = (0 + maxProfitTopDown(prices, i+1, 0, dp));
-            profit = Math.max(sellThis, skipThis);
-        }
-        return dp[i][buy] = profit;
-    }
-
     public int maxProfit(int[] prices) {
-        //1. Recursion
-        //return maxProfitHelper(prices, 0, 1);
-
-        //2. DP Top Down
-        // int n = prices.length;
-        // int[][] dp = new int[n][2];
-        // for(int[] row : dp)
-        //     Arrays.fill(row, -1);
-        
-        // return maxProfitTopDown(prices, 0, 1, dp);
-
-        //3. DP Bottom Up
-        int n = prices.length;
-        int[][] dp = new int[n+1][2];
-        for(int i=n-1; i>=0; i--) {
-            for(int buy=0; buy<=1; buy++) {
-                int profit = 0;
-                if(buy == 1) {
-                    //Its a buy call , we can buy or skip
-                    int buyThis = (-prices[i] + dp[i+1][0]);
-                    int skipThis = (0 + dp[i+1][1]);
-                    profit = Math.max(buyThis, skipThis);
-                } else {
-                    //Its a sell call, we can sell of skip 
-                    int sellThis = (+prices[i] + dp[i+1][1]);
-                    int skipThis = (0 + dp[i+1][0]);
-                    profit = Math.max(sellThis, skipThis);
-                }
-                dp[i][buy] = profit;
-            }
-        }
-        return dp[0][1];
+        int[][] dp = new int[prices.length+1][2];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        return helper(prices, 0, 1, dp);
     }
 }
