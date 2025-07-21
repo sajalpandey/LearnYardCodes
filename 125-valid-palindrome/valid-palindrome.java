ce6fc48bf40559bf38d1ce90312140f8862d4efd
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2025-07-22 00:20:43 +0530

    Time: 3 ms (72.89%) | Memory: 43.3 MB (61.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,19 @@
 class Solution {
-    public boolean isCharacter(char ch) {
-        return ch >= 'a' && ch <= 'z' || ch >= '0' && ch <='9';
-    }
     public boolean isPalindrome(String s) {
-        char[] arr = s.toLowerCase().toCharArray();;
+        if(s == null)
+            return true;
+        char[] arr = s.trim().toLowerCase().toCharArray();
         int i=0, j = arr.length-1;
-        while(i <= j) {
-            System.out.println(arr[i]+" -- "+arr[j]);
-            if(isCharacter(arr[i]) && isCharacter(arr[j])) {
-                if(arr[i] != arr[j])
-                    return false;
-                i++; j--;
-            } else if(!isCharacter(arr[i]))
+        while(i < j) {
+            while(i < j && !Character.isLetterOrDigit(arr[i]))
                 i++;
-            else if(!isCharacter(arr[j]))
+            while(i<j && !Character.isLetterOrDigit(arr[j]))
                 j--;
+            if(arr[i] != arr[j])
+                return false;
+            
+            i++;
+            j--;
         }
         return true;
     }
