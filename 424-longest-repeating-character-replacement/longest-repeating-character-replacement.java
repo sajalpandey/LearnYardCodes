cc157d455ecb3220ea92646248e8af47d1aeb7c7
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2025-07-28 23:35:48 +0530

    Time: 11 ms (60.67%) | Memory: 44.7 MB (82.99%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // changes = window_length - max freq character
-        // if these changes > k
-        // we need to shrink our window
-        
-        int maxFreq = 0;
-        int i = 0, j = 0;
-        int[] freq = new int[26];
-        
-        int result = 0;
+        // Longest substring with k distinct character
+        int map[] = new int[26];
+        int i = 0, j = 0, maxCount = 0, ans = 0;
         while(j < s.length()) {
             char ch = s.charAt(j);
-            maxFreq = Math.max(maxFreq, ++freq[ch - 'A']);
-            
-            int changes = (j - i + 1) - maxFreq;
-            
+            map[ch - 'A']++;
+            maxCount = Math.max(maxCount, map[ch - 'A']);
+
+            int windowSize = j - i + 1;
+            int changes = windowSize - maxCount;
             if(changes > k) {
-                // we need to shrink our window
-                freq[s.charAt(i) - 'A']--;
+                map[s.charAt(i) - 'A']--;
                 i++;
             }
-            
-            result = Math.max(result, (j-i+1));
+            ans = Math.max(ans , j - i + 1);
             j++;
         }
-        
-        return result;
+        return ans;
+
     }
 }
