8e5c87f2dc47093921c976c772702dc730f39970
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2025-08-03 22:22:28 +0530

    Time: 4 ms (98.93%) | Memory: 42.6 MB (79.41%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,6 +1,5 @@
 class Solution {
-
-    public boolean checkAllZero(int[] freq){
+    public boolean checkAllZero(int [] freq) {
         for(int i=0; i<freq.length; i++) {
             if(freq[i] != 0)
                 return false;
@@ -8,30 +7,29 @@ class Solution {
         return true;
     }
     public boolean checkInclusion(String s1, String s2) {
-        //Permutaion means all characters of s1 should be present in s2
-        //store all characters in freq array
-        //for each window of size(s1) in s2 find if it contains all chars of s1
-
-        //TC : O(|s2|)
-
+        if(s1.length() > s2.length())
+            return false;
         int[] freq = new int[26];
         for(char ch : s1.toCharArray()) {
             freq[ch - 'a']++;
         }
-
-        for(int i=0; i<s2.length(); i++) {
-            char ch = s2.charAt(i);
-            freq[ch - 'a']--;
-
-            if(i >= s1.length()) {
-                freq[s2.charAt(i-s1.length()) - 'a']++;
-            }
-            if(checkAllZero(freq))
-                    return true;
+        int i = 0, j = 0;
+        while(j < s1.length()) {
+            freq[s2.charAt(j) - 'a']--;
+            j++;
         }
 
-
+        if(checkAllZero(freq)) {
+            return true;
+        }
+        while(j < s2.length()) {
+            freq[s2.charAt(j) - 'a']--;
+            freq[s2.charAt(i++) - 'a']++;
+            if(checkAllZero(freq)) {
+                return true;
+            }
+            j++;
+        }
         return false;
-
     }
 }
