ea2439aef47cb08edf98680f64a474e45e0b042f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2025-08-11 22:42:35 +0530

    Time: 31 ms (67.01%) | Memory: 65.1 MB (5.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        //First approach is check every window of size k TC(n*k)
-        //Optimized is using deque
+        int size = nums.length - k + 1;
+        int[] result = new int[size];
         Deque<Integer> dq = new LinkedList<>();
-        int n = nums.length;
-        int[] result = new int[n-k+1];
-        int p = 0, i = 0;
-        while(i < n) {
-            //Logic how to keep in Dq
-            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
+        //idea here is to keep indexes in deque, ele which is max shoudl be in front 
+        //with in window index only
+        int p = 0;
+        for(int i=0; i<nums.length; i++) {
+            int ele = nums[i];
+            while(!dq.isEmpty() && nums[dq.peekLast()] < ele) {
                 dq.removeLast();
-
-            //Logic when to remove from dq
-            while(!dq.isEmpty() && dq.peekFirst() <= (i-k))
+            }
+            if(!dq.isEmpty() && dq.peekFirst() <= i-k) {
                 dq.removeFirst();
-
+            }
             dq.addLast(i);
-            //update result
-            if(i >= k-1)
+            if(i >= k - 1) {
                 result[p++] = nums[dq.peekFirst()];
-            i++;
+            }
         }
-
         return result;
     }
 }
